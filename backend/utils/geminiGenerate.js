628491cbe695a64ad31231a51b587b9dedde0320
Aman Kishor <pk2664959@gmail.com>
2025-08-30 14:12:06 +0530

    added env in the utils

--- backend/utils/geminiGenerate.js
+++ backend/utils/geminiGenerate.js
@@ -1,6 +1,7 @@
 const { GoogleGenAI } = require("@google/genai");
+require("dotenv").config();
 const ai = new GoogleGenAI({
-  apiKey: "AIzaSyBWZzlHNfuNKvmBZU3Nq7gVGCMSSbHo9aM",
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
 const generateWithGemini = async (name, jobTitle, experience, skills) => {
